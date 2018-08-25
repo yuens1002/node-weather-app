d5fb15ef31d2685f8a3150f512337e82d449fcfa
sunny yuen <yuens1002@gmail.com>
2018-08-25 07:57:08 -0400

    chain 2 api requets with axios promises and error handling

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const darksky = require('./darkSky/darkSkyAPI');
+const axios = require('axios');
+
+const MQurl = 'http://www.mapquestapi.com/geocoding/v1/address?';
+const MQkey = '0aSgK7RwJNt2FH7Zaq65yPQYKlywr3QY';
+const MQurlStr = `${MQurl}key=${MQkey}&location=`;
+const DSkey = 'b431925c1daa955d88b36b219e6fb858';
+const DSurl = 'https://api.darksky.net/forecast/';
+const results = {};
+
 
 const argv = yargs
   .options({
@@ -21,8 +28,27 @@ if (argv.a === '') {
   return;
 }
 
-geocode.getGeocode(argv.a).then (res => {
-  return darksky.getWeather(res);
-}).then (res => {
-  console.log(`the temperature at ${res.address.street} ${res.address.city}, ${res.address.state} ${res.address.zip} is ${res.weather.temperature} F currently, but it feels like ${res.weather.apparentTemperature} F`)
-}).catch (error => console.log(error));
+axios.get(MQurlStr+argv.a).then(res => {
+  results.mqRes = {
+    Address: {
+      street: res.data.results[0].locations[0].street,
+      city: res.data.results[0].locations[0].adminArea5,
+      state: res.data.results[0].locations[0].adminArea3,
+      zip: res.data.results[0].locations[0].postalCode
+    },
+    Latitude: res.data.results[0].locations[0].latLng.lat,
+    Longitude: res.data.results[0].locations[0].latLng.lng
+  };
+  return axios.get(`${DSurl}${DSkey}/${results.mqRes.Latitude},${results.mqRes.Longitude}`);
+}).then(res => {
+  results.dsRes = {
+    weather: res.data.currently
+  };
+  console.log(`the temperature at ${results.mqRes.Address.street} ${results.mqRes.Address.city}, ${results.mqRes.Address.state} ${results.mqRes.Address.zip} is ${results.dsRes.weather.temperature} F currently, but it feels like ${results.dsRes.weather.apparentTemperature} F`)
+}).catch(error => {
+  if (error.code === 'ENOTFOUND') {
+    console.log('can\'t connect to api' + ' ' + error.host);
+  } else {
+    console.log(`check the api key used here: ${error.response.config.url}`);
+  }
+});
